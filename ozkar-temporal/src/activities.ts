001bd938e9abaf6d73d5c631c0091bcaabe6684c
robi <robins07@me.com>
2024-02-22 23:55:08 +0100

    fhir workflow demo

--- ozkar-temporal/src/activities.ts
+++ ozkar-temporal/src/activities.ts
@@ -17,7 +17,8 @@ export type ProofRequest = {
   resource: FhirResource;
 };
 
-import { Field } from 'o1js';
+import { Field, Proof } from 'o1js';
+import { ExecutionProof } from 'ozkar-zerkle/build/esm/lib/mina-backend';
 
 export async function computeProof(proofRequest: ProofRequest): Promise<ProofRequest> {
   //console.log(IPLD.LinearModel.fromJS(proofRequest));
@@ -57,4 +58,35 @@ export async function computeProof(proofRequest: ProofRequest): Promise<ProofReq
   console.log('q', q);
   console.log(map);
   return proofRequest;
+}
+
+export async function computeFhirProof(proofRequest: ProofRequest): Promise<ExecutionProof> {
+  //console.log(IPLD.LinearModel.fromJS(proofRequest));
+  const map = MerkleMapFactory.fromLinearModel(IPLD.LinearModel.fromJS(proofRequest.resource));
+  // // Incoming public stuff
+  const q = ZkQuery.parse(proofRequest.query);
+
+  console.time('compile');
+  const backend = await Backend.compile();
+  console.timeEnd('compile');
+
+  console.time('execute.0');
+  const proofE = await backend.proveQuery(q).run(map);
+  console.timeEnd('execute.0');
+  console.log(
+    'p.0',
+    proofE.publicOutput.trace.toString(),
+    proofE.publicOutput.isSatisfied.toString(),
+    proofE.publicInput.given.toString(),
+    proofE.publicInput.root.toString()
+  );
+
+  console.time('execute.1');
+  const proof = await backend.execute(map, q);
+  const verification = await Verification.check(q, proof);
+  console.timeEnd('execute.1');
+  console.log('a', proof, verification);
+  console.log('q', q);
+  console.log(map);
+  return proof;
 }
