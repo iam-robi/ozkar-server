67820be19e599d414c5cef7defd4b61ea4b712df
robi <robins07@me.com>
2024-02-25 21:38:20 +0100

    :feature: fetch workflow status/ description

--- src/proofs/proofs.resolver.ts
+++ src/proofs/proofs.resolver.ts
@@ -1,23 +1,15 @@
 import { PrismaService } from 'nestjs-prisma';
-import {
-  Resolver,
-  Query,
-  Parent,
-  Args,
-  ResolveField,
-  Subscription,
-  Mutation,
-} from '@nestjs/graphql';
+import { Resolver, Query, Args, Subscription } from '@nestjs/graphql';
 import { PubSub } from 'graphql-subscriptions';
 
-import { UseGuards } from '@nestjs/common';
-
+//import { UseGuards } from '@nestjs/common';
+import { type WorkflowExecutionDescription } from '@temporalio/client';
 import { Proof } from './models/proof.model.js';
 
 import { CreateProofInput } from './dto/createProof.input.js';
 import GraphQLJSON from 'graphql-type-json';
 import { ProofService } from './proofs.service.js';
-
+import { WorflowsStatusInput } from './dto/workflowStatus.input.js';
 const pubSub = new PubSub();
 
 @Resolver(() => Proof)
@@ -45,5 +37,16 @@ export class ProofsResolver {
     }
 
     return workflowIds;
+  }
+
+  @Query(() => GraphQLJSON)
+  async getWorflowStatus(
+    @Args('workflowIds') workflowIds: WorflowsStatusInput,
+  ): Promise<Array<WorkflowExecutionDescription>> {
+    const workflowDescriptions = this.proofService.getWorkflowsStatus(
+      workflowIds.workflowIds,
+    );
+
+    return workflowDescriptions;
   }
 }
--- src/proofs/proofs.service.ts
+++ src/proofs/proofs.service.ts
@@ -1,6 +1,10 @@
 import { PrismaService } from 'nestjs-prisma';
 import { Injectable } from '@nestjs/common';
-import { WorkflowClient, Connection } from '@temporalio/client';
+import {
+  WorkflowClient,
+  Connection,
+  type WorkflowExecutionDescription,
+} from '@temporalio/client';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
@@ -17,13 +21,36 @@ export class ProofService {
       namespace: this.config.get<string>('temporal.namespace'),
     });
 
-    const handle = await client.start('fhirProof', {
+    const handle = await client.start('proveFhir', {
       args: [proofRequest],
-      taskQueue: 'fhir:dev',
+      taskQueue: 'compute-proof-request',
       workflowId: 'fhir-' + crypto.randomUUID(),
     });
 
     return handle.workflowId;
+  }
+
+  async getWorkflowsStatus(
+    workflowIds: Array<string>,
+  ): Promise<Array<WorkflowExecutionDescription>> {
+    const connection = await Connection.connect({
+      address: this.config.get<string>('temporal.address'),
+    });
+
+    const client = new WorkflowClient({
+      connection,
+      namespace: this.config.get<string>('temporal.namespace'),
+    });
+
+    const workflowDescriptions: Array<WorkflowExecutionDescription> = [];
+
+    for (const workflowId of workflowIds) {
+      const handle = client.getHandle(String(workflowId));
+      const workflowDescription = await handle.describe();
+      workflowDescriptions.push(workflowDescription);
+    }
+
+    return workflowDescriptions;
   }
 
   formatQuery(query: string) {
